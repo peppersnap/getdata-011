64c02577b4df65183506e12b9e27b71f8eb4aaf5
Peppersnap <peppersnap.uk@gmail.com>
2015-02-15 16:05:01 +0000

    Initial commit
    Conflicts:
    	run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,28 @@
-require(dplyr)
-#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-#              destfile="dataset.zip", mode="wb")
-#unzip("dataset.zip")
+# This script assumes the dataset has already been downloaded and
+# unzipped into the working directory:
+#
+# download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+#               destfile="dataset.zip", mode="wb")
+# unzip("dataset.zip")
+
+# read the feature labels (only the 2nd column, as we don't need the
+# row number/ID)
 
 features <- read.table("UCI HAR Dataset/features.txt",
-                       col.names=c("Feature.ID", "Feature"),
-                       colClasses=c("factor","factor"))
+                       col.names=c(NA, "Feature"),
+                       colClasses=c(NA,"factor"))[,2]
 
+# read the activity labels. We keep the ID here as we'll use it to merge
+# with the feature records later
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt",
                               col.names=c("Activity.ID", "Activity"),
                               colClasses=c("factor", "factor"))
 
-# get the test data
-X_test <- read.table("UCI HAR Dataset/test/X_test.txt",
-                     col.names=features$Feature)
+###############################################################################
 
-#label this data as "test"
-test.data <- cbind(c("test"),X_test)
-names(test.data)[1] <- "dataset"
+# get the test feature records
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt",
+                     col.names=features)
 
 # get activity identifiers for the test data
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt",
@@ -26,18 +31,16 @@ y_test <- read.table("UCI HAR Dataset/test/y_test.txt",
 # get subject identifiers for the test data
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt",
                            col.names=c("Subject.ID"),
-                           colClasses=c("factor"))
-g
+                           colClasses=c("integer"))
+
 # Combine the activity IDs and subject IDs with the test data set
 test.data <- cbind(y_test, X_test, subject_test)
 
+###############################################################################
+
 # get the training data
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt",
-                     col.names=features$Feature)
-
-#label this data as "train"
-train.data <- cbind(c("train"),X_test)
-names(train.data)[1] <- "dataset"
+                     col.names=features)
 
 # get activity identifiers to the training data
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt",
@@ -51,6 +54,8 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt",
 # Combine the activity IDs and subject IDs with the training data set
 train.data <- cbind(y_train, X_train, subject_train)
 
+###############################################################################
+
 # Combine test and training data into one dataset 
 all.data <- rbind(test.data, train.data)
 
@@ -60,24 +65,17 @@ all.data <- rbind(test.data, train.data)
 # the tables based on this column.
 all.data <- merge(activity_labels, all.data)
 
-# Extract only the measurements on the mean and standard deviation.
-# 1) get the names of columns that contain means,
-# i.e. the name contains '.mean'
-mean.colnames <- names(all.data)[grep(".mean",names(all.data))]
+# Extract only the measurements on the mean and standard deviation, i.e.
+# that contain the text '.mean.' or '.std.'
+required.colnames <- names(all.data)[grepl("\\.mean\\.|\\.std\\.",names(all.data))]
 
-# 2) get the names of columns that contain standard deviations,
-# i.e. the name contains '.std'
-std.colnames <- names(all.data)[grep(".std",names(all.data))]
+# create a dataset of the means of the data, grouped by Activity and Subject
+tidy <- aggregate(all.data[,required.colnames],
+                  by=list(Activity=all.data$Activity,
+                          Subject=all.data$Subject.ID), mean)
 
-# 3) Combine these, and include the subject ID and activity column names
-required.colnames <- c(mean.colnames, std.colnames)
+# sort the data by Activity and Subject
+tidy <- tidy[order(tidy$Activity, as.integer(tidy$Subject)),]
 
-# Take a subset of the all.data dataframe including only the previously
-# specified columns
-factor.colnames <- c("Activity", "Subject.ID")
-
-subset.data <- all.data[,c(factor.colnames, required.colnames)]
-
-mean.data <- by(subset.data[,required.colnames],subset.data[,factor.colnames],
-                colMeans)
-
+# output the data to CSV
+write.csv(tidy, "tidy.csv", row.names = FALSE)
